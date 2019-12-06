397ea108f49674faf73a1c1628267301529213bc
Pedro <pedrodelrosario1@gmail.com>
2019-12-06 19:26:49 -0400

    First One

--- tests/tests.js
+++ tests/tests.js
@@ -4,42 +4,122 @@ const employee = require('./employee')
 const products = require('./products')
 const pricing = require('../pricing')
 
-describe('calculateProductPrice', () => {
-  it('returns the price for a voluntary life product for a single employee', () => {
-    const selectedOptions = {
-      familyMembersToCover: ['ee'],
-      coverageLevel: [{ role: 'ee', coverage: 125000 }],
-    }
-    const price = pricing.calculateProductPrice(products.voluntaryLife, employee, selectedOptions)
+describe('pricing', () => {
+  describe('formatPrice', () => {
+    it('returns the price given, truncated to two decimal places', () => {
+      const formattedPrice = pricing.formatPrice(15.335)
 
-    expect(price).to.equal(39.37)
+      expect(formattedPrice).to.equal(15.33)
+    })
+
+    it('returns the price with two decimal places when a whole number is provided', () => {
+      const formattedPrice = pricing.formatPrice(15)
+
+      expect(formattedPrice).to.equal(15.00)
+    })
   })
 
-  it('returns the price for a voluntary life product for an employee with a spouse', () => {
-    const selectedOptions = {
-      familyMembersToCover: ['ee', 'sp'],
-      coverageLevel: [
-        { role: 'ee', coverage: 200000 },
-        { role: 'sp', coverage: 75000 },
-      ],
-    }
-    const price = pricing.calculateProductPrice(products.voluntaryLife, employee, selectedOptions)
+  describe('getEmployerContribution', () => {
+    it('returns the contribution amount from the product when contribution type is dollars', () => {
+      const contribution = pricing.getEmployerContribution(products.longTermDisability.employerContribution, 15.33)
 
-    expect(price).to.equal(71.09)
+      expect(contribution).to.equal(10)
+    })
+
+    it('returns the calculated contribution (percent of price) when the contribution type is percentage', () => {
+      const contribution = pricing.getEmployerContribution(products.voluntaryLife.employerContribution, 150)
+
+      expect(contribution).to.equal(15)
+
+    })
   })
 
-  it('returns the price for a disability product for an employee', () => {
-    const selectedOptions = {
-      familyMembersToCover: ['ee']
-    }
-    const price = pricing.calculateProductPrice(products.longTermDisability, employee, selectedOptions)
+  describe('calculateVolLifePricePerRole', () => {
+    it('retuns the price of vol life coverage for a particular role (ee)', () => {
+      const coverageLevel = [{ role: 'ee', coverage: 125000 }]
 
-    expect(price).to.equal(22.04)
+      const price = pricing.calculateVolLifePricePerRole('ee', coverageLevel, products.voluntaryLife.costs)
+
+      expect(price).to.equal(43.75)
+    })
   })
 
-  it('throws an error on unknown product type', () => {
-    const unknownProduct = { type: 'vision' }
+  describe('calculateVolLifePrice', () => {
+    it('returns the price (pre employer contribution) of a vol life product for employee only coverage', () => {
+      const selectedOptions = {
+        familyMembersToCover: ['ee'],
+        coverageLevel: [{ role: 'ee', coverage: 125000 }],
+      }
 
-    expect(() => pricing.calculateProductPrice(unknownProduct, {}, {})).to.throw('Unknown product type: vision')
+      const price = pricing.calculateVolLifePrice(products.voluntaryLife, selectedOptions)
+
+      expect(price).to.equal(43.75)
+    })
+
+    it('returns the price (pre employer contribution) of a vol life product for employee and spouse coverage', () => {
+      const selectedOptions = {
+        familyMembersToCover: ['ee', 'sp'],
+        coverageLevel: [
+          { role: 'ee', coverage: 200000 },
+          { role: 'sp', coverage: 75000 },
+        ],
+      }
+
+      const price = pricing.calculateVolLifePrice(products.voluntaryLife, selectedOptions)
+
+      expect(price).to.equal(79)
+    })
+  })
+
+  describe('calculateLTDPrice', () => {
+    it('returns the price (pre employer contribution) for a LTD product for an employee', () => {
+      const selectedOptions = {
+        familyMembersToCover: ['ee']
+      }
+
+      const price = pricing.calculateLTDPrice(products.longTermDisability, employee, selectedOptions)
+
+      expect(price).to.equal(32.04)
+    })
+  })
+
+  describe('calculateProductPrice', () => {
+    it('returns the price for a voluntary life product for a single employee', () => {
+      const selectedOptions = {
+        familyMembersToCover: ['ee'],
+        coverageLevel: [{ role: 'ee', coverage: 125000 }],
+      }
+      const price = pricing.calculateProductPrice(products.voluntaryLife, employee, selectedOptions)
+
+      expect(price).to.equal(39.37)
+    })
+
+    it('returns the price for a voluntary life product for an employee with a spouse', () => {
+      const selectedOptions = {
+        familyMembersToCover: ['ee', 'sp'],
+        coverageLevel: [
+          { role: 'ee', coverage: 200000 },
+          { role: 'sp', coverage: 75000 },
+        ],
+      }
+      const price = pricing.calculateProductPrice(products.voluntaryLife, employee, selectedOptions)
+
+      expect(price).to.equal(71.09)
+    })
+
+    it('returns the price for a disability product for an employee', () => {
+      const selectedOptions = {
+        familyMembersToCover: ['ee']
+      }
+      const price = pricing.calculateProductPrice(products.longTermDisability, employee, selectedOptions)
+
+      expect(price).to.equal(22.04)
+    })
+
+    it('throws an error on unknown product type', () => {
+      const unknownProduct = { type: 'vision' }
+
+      expect(() => pricing.calculateProductPrice(unknownProduct, {}, {})).to.throw('Unknown product type: vision')
+    })
   })
 })
